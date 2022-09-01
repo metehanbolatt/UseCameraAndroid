bdce5a3b8efc99f52dc416544d5adb34feb4547f
Metehan <metehan.bolat.ie@gmail.com>
2022-09-02 01:15:48 +0300

    Camera app was completed with deprecated code. (startActivityForResult)

--- app/src/main/java/com/metehanbolat/usecameraandroid/MainActivity.kt
+++ app/src/main/java/com/metehanbolat/usecameraandroid/MainActivity.kt
@@ -1,10 +1,20 @@
 package com.metehanbolat.usecameraandroid
 
+import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import com.metehanbolat.usecameraandroid.databinding.ActivityMainBinding
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), EasyPermissions.PermissionCallbacks {
+
+    companion object {
+        const val CAMERA_PERMISSION_REQUEST_CODE = 100
+        const val IMAGE_CAPTURE_REQUEST_CODE = 101
+    }
 
     private lateinit var binding: ActivityMainBinding
 
@@ -14,5 +24,55 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        requestCameraPermission()
+
+        binding.cameraButton.setOnClickListener {
+            Intent(MediaStore.ACTION_IMAGE_CAPTURE).apply {
+                startActivityForResult(intent, 101)
+            }
+        }
+
+    }
+
+    private fun requestCameraPermission() {
+        if (hasCameraPermission()) return
+
+        EasyPermissions.requestPermissions(
+            this,
+            "You need camera permission to use this app.",
+            CAMERA_PERMISSION_REQUEST_CODE,
+            android.Manifest.permission.CAMERA
+        )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == IMAGE_CAPTURE_REQUEST_CODE && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            binding.cameraImage.setImageBitmap(imageBitmap)
+        }
+    }
+
+    private fun hasCameraPermission() = EasyPermissions.hasPermissions(this, android.Manifest.permission.CAMERA)
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
+
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            AppSettingsDialog.Builder(this).build().show()
+        } else {
+            requestCameraPermission()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult((requestCode), permissions, grantResults, this)
     }
 }
